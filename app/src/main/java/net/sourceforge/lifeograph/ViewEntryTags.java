e4639d056305e00a976b32b5a1f5786b52f7c851
Ahmet Ozturk <aoz_2@yahoo.com>
2016-10-22 17:11:01 +0300

    improved the look of the tag widget by simplifying and using the overall theme colors for not themed tags

--- app/src/main/java/net/sourceforge/lifeograph/ViewEntryTags.java
+++ app/src/main/java/net/sourceforge/lifeograph/ViewEntryTags.java
@@ -35,16 +35,14 @@ import android.view.ViewGroup;
 
 public class ViewEntryTags extends View implements GestureDetector.OnGestureListener
 {
-    // CONSTANTS
-    static final float      MARGIN = 18f;
-    static final float      HSPACING = 15f;
-    static final float      VSPACING = 14f;
-    static final float      LABEL_OFFSET = 2f;
-    static final float      TEXT_HEIGHT = 25f; // different in Android
-    static final float      ITEM_BORDER = 4f;  // must be < MARGIN
-    static final int        ICON_SIZE = 16;
-    static final float      ITEM_HEIGHT =  40f;
-    static final float      HALF_HEIGHT = 20f;
+    // CONSTANTS (different in Android)
+    static final float MARGIN = 14f;
+    static final float HSPACING = 12f;
+    static final float VSPACING = 20f;
+    static final float TEXT_HEIGHT = 35f;
+    static final float TEXT_PADDING = 5f;
+    static final float ITEM_HEIGHT =  TEXT_HEIGHT + ( 2 * TEXT_PADDING );
+    static final float HALF_HEIGHT = ITEM_HEIGHT/2;
 
     public ViewEntryTags( Context c, AttributeSet attrs ) {
         super( c, attrs );
@@ -62,8 +60,6 @@ public class ViewEntryTags extends View implements GestureDetector.OnGestureList
         mPaint.setStrokeWidth( 4f );
 
         mGestureDetector = new GestureDetector( c, this );
-
-        m_color_text_default = Color.BLACK;
     }
 
     public void setListener( Listener listener ) {
@@ -97,35 +93,32 @@ public class ViewEntryTags extends View implements GestureDetector.OnGestureList
 
     void add_item( Canvas canvas, TagItem ti ) {
         float text_width = mPaint.measureText( ti.label );
-        if( m_pos_x + ICON_SIZE + LABEL_OFFSET + text_width + MARGIN > m_width ) {
+        if( m_pos_x + ( 2 * TEXT_PADDING ) + text_width + HALF_HEIGHT + MARGIN > m_width ) {
             m_pos_x = MARGIN;
             m_pos_y += ( ITEM_HEIGHT + VSPACING );
             mDesiredHeight += ( ITEM_HEIGHT + VSPACING );
         }
 
-        ti.xl = m_pos_x - ITEM_BORDER;
-        ti.xr = m_pos_x + text_width + HALF_HEIGHT;
-        ti.yl = m_pos_y - ITEM_BORDER;
-        ti.yr = ti.yl + ITEM_HEIGHT;
-
-        if( ti.tag == null )
-            ti.xr += ( ICON_SIZE + LABEL_OFFSET );
+        ti.xl = m_pos_x - HSPACING / 2;
+        ti.xr = ti.xl + TEXT_PADDING + text_width + TEXT_PADDING + HALF_HEIGHT + HSPACING;
+        ti.yl = m_pos_y - VSPACING / 2;
+        ti.yr = ti.yl + ITEM_HEIGHT + VSPACING;
 
         mPath.reset(); // reset path
 
         // BACKGROUND
         if( ti.hovered || ti.tag != null ) {
-            float width = ( ti.xr - ti.xl - HALF_HEIGHT );
+            float h_width = ( TEXT_PADDING + text_width + TEXT_PADDING );
 
-            mPath.moveTo( ti.xl, ti.yl );
-            mPath.rLineTo( width, 0.0f );
+            mPath.moveTo( m_pos_x, m_pos_y );
+            mPath.rLineTo( h_width, 0.0f );
             mPath.rLineTo( HALF_HEIGHT, HALF_HEIGHT );
             mPath.rLineTo( HALF_HEIGHT * -1, HALF_HEIGHT );
-            mPath.rLineTo( -width, 0 );
+            mPath.rLineTo( -h_width, 0 );
             mPath.close();
 
             if( ti.hovered && m_flag_editable )
-                mPaint.setStrokeWidth( 5.0f );
+                mPaint.setStrokeWidth( 6.0f );
 
             if( ti.tag != null && ti.tag.get_has_own_theme() ) {
                 mPaint.setColor( ti.tag.get_theme().color_base );
@@ -141,36 +134,30 @@ public class ViewEntryTags extends View implements GestureDetector.OnGestureList
                 mPaint.setPathEffect( null );
             }
             else {
+                mPaint.setColor( Color.WHITE );
+                canvas.drawPath( mPath, mPaint );
                 mPaint.setStyle( Paint.Style.STROKE );
-                mPaint.setColor( m_color_text_default );
                 canvas.drawPath( mPath, mPaint );
             }
             mPaint.setStyle( Paint.Style.FILL );
 
             if( ti.hovered && m_flag_editable )
-                mPaint.setStrokeWidth( 2.0f );  // restore the stroke width
+                mPaint.setStrokeWidth( 1.0f );  // restore the stroke width
         }
 
-        // ICON
-//        if( tag == m_add_tag_item ) {
-//            cr->set_source( m_image_surface_add, m_pos_x, m_pos_y );
-//            cr->rectangle( m_pos_x, m_pos_y, ICON_SIZE, ICON_SIZE );
-//            cr->clip();
-//            cr->paint();
-//            cr->reset_clip();
-//
-//            m_pos_x += ( ICON_SIZE + LABEL_OFFSET );
-//        }
-
         // LABEL
-        if( ti.tag != null && ti.tag.get_has_own_theme() )
+        if( ti.tag == null )
+            mPaint.setColor( ti.hovered ? m_color_text_default : Color.WHITE );
+        else if( ti.tag.get_has_own_theme() )
             mPaint.setColor( ti.tag.get_theme().color_text );
         else
             mPaint.setColor( m_color_text_default );
 
-        canvas.drawText( ti.label, m_pos_x, m_pos_y + TEXT_HEIGHT, mPaint );
+        canvas.drawText( ti.label, m_pos_x + TEXT_PADDING, m_pos_y + TEXT_HEIGHT, mPaint );
+        // NOTE: TEXT_PADDING is not added to the y coordinate making text doubly padded at the
+        // bottom as this produces a better result
 
-        m_pos_x += ( text_width + HALF_HEIGHT + HSPACING );
+        m_pos_x += ( TEXT_PADDING + text_width + TEXT_PADDING + HALF_HEIGHT + HSPACING );
     }
 
     // override onSizeChanged
@@ -192,7 +179,7 @@ public class ViewEntryTags extends View implements GestureDetector.OnGestureList
         mDesiredHeight = ( int ) ( MARGIN + ITEM_HEIGHT + MARGIN );
         mPaint.setTextSize( TEXT_HEIGHT );
         mPaint.setStyle( Paint.Style.FILL );
-        mPaint.setStrokeWidth( 2.0f );
+        mPaint.setStrokeWidth( 1.0f );
 
         if( m_items.isEmpty() && !m_flag_editable ) {
             mPaint.setColor( m_color_text_default );
@@ -263,7 +250,7 @@ public class ViewEntryTags extends View implements GestureDetector.OnGestureList
 
     // DATA
     private Entry m_ptr2entry = null;
-    private int m_color_text_default;
+    private int m_color_text_default = getResources().getColor( R.color.t_mid );
 
     class TagItem
     {
