7acca3f5f2b58486562933d9a48d99890066564e
Ahmet Ozturk <aoz_2@yahoo.com>
2016-12-12 22:20:51 +0300

    updated jni, fixed reading encrypted files.

--- app/src/main/jni/lifeocrypt.c
+++ app/src/main/jni/lifeocrypt.c
@@ -1,6 +1,6 @@
 /***********************************************************************************
 
-    Copyright (C) 2007-2015 Ahmet Öztürk (aoz_2@yahoo.com)
+    Copyright (C) 2007-2016 Ahmet Öztürk (aoz_2@yahoo.com)
 
     This file is part of Lifeograph.
 
@@ -41,39 +41,52 @@ Java_net_sourceforge_lifeograph_Diary_decryptBuffer( JNIEnv* env,
                                                      jbyteArray j_iv )
 {
     unsigned char* key;
-    const char* passphrase = ( *env )->GetStringUTFChars( env, j_passphrase, NULL );
-    jbyte* salt = ( *env )->GetByteArrayElements( env, j_salt, NULL );
+    const char* passphrase = ( char* ) ( *env )->GetStringUTFChars( env, j_passphrase, NULL );
+    const char* salt = ( char* ) ( *env )->GetByteArrayElements( env, j_salt, NULL );
 
-    Cipher_expand_key( passphrase, salt, &key );
+    Cipher_expand_key( passphrase, ( unsigned char* ) salt, &key );
 
-    jbyte* buffer = ( *env )->GetByteArrayElements( env, j_buffer, NULL );
-    jbyte* iv = ( *env )->GetByteArrayElements( env, j_iv, NULL );
+    char* buffer = ( char* ) ( *env )->GetByteArrayElements( env, j_buffer, NULL );
+    char* iv = ( char* ) ( *env )->GetByteArrayElements( env, j_iv, NULL );
 
-    Cipher_decrypt_buffer( buffer, size, key, iv );
+    Cipher_decrypt_buffer( ( unsigned char* ) buffer, size, key, iv );
 
     free( key );
+    ( *env )->ReleaseByteArrayElements( env, j_salt, salt, JNI_ABORT );
+    ( *env )->ReleaseByteArrayElements( env, j_iv, iv, JNI_ABORT );
+    ( *env )->ReleaseByteArrayElements( env, j_buffer, buffer, JNI_ABORT );
 
-    ( *env )->ReleaseByteArrayElements( env, j_salt, salt, 0 );
-    ( *env )->ReleaseByteArrayElements( env, j_iv, iv, 0 );
+    int size_dec_buf = 0;
+    int round = 0;
 
-    /*jbyteArray j_buffer_out = ( *env )->NewByteArray( env, size );  // allocate
-    if( !j_buffer_out )
-        return NULL;
+    // EOF DETECTION: RATHER UGLY CODE
+    for( ; size_dec_buf < size - 1; size_dec_buf++ )
+    {
+        if( buffer[ size_dec_buf ] == '\n' && buffer[ size_dec_buf + 1 ] =='\n' )
+        {
+            if( round > 0 && size_dec_buf < size - 3 &&
+                ( buffer[ size_dec_buf + 2 ] != 'I' ||  buffer[ size_dec_buf + 3 ] != 'D' ) )
+            {
+                size_dec_buf += 2;
+                break;
+            }
+            else
+                round++;
+        }
+    }
 
-    ( *env )->SetByteArrayRegion( env, j_buffer_out, 0, size, buffer );  // copy
-
-    ( *env )->ReleaseByteArrayElements( env, j_buffer, buffer, 0 );
-
-    return j_buffer_out;*/
+    char* dec_buf = malloc( size_dec_buf );
+    memcpy( dec_buf, buffer, size_dec_buf - 1 );
+    dec_buf[ size_dec_buf - 1 ] = 0; // terminating zero
 
     jstring output;
     // cannot check the '\n' due to multi-byte char case
-    if( buffer[ 0 ] == passphrase[ 0 ] ) //&& buffer[ 1 ] == '\n' )
-        output = ( *env )->NewStringUTF( env, buffer );
+    if( dec_buf[ 0 ] == passphrase[ 0 ] ) //&& buffer[ 1 ] == '\n' )
+        output = ( *env )->NewStringUTF( env, dec_buf );
     else
         output = ( *env )->NewStringUTF( env, "XX" );
 
-    ( *env )->ReleaseByteArrayElements( env, j_buffer, buffer, 0 );
+    free( dec_buf );
 
     return output;
 }
