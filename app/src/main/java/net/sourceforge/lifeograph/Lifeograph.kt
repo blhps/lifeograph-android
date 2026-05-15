7fb5b7ae15dda88b57fe691beafc40286bb76747
Ahmet Ozturk <aoz_2@yahoo.com>
2026-05-15 23:00:26 +0300

    updated dependency version + minor cleanup.

--- app/src/main/java/net/sourceforge/lifeograph/Lifeograph.kt
+++ app/src/main/java/net/sourceforge/lifeograph/Lifeograph.kt
@@ -25,7 +25,6 @@ import android.app.AlertDialog
 import android.app.Application
 import android.content.Context
 import android.content.DialogInterface
-import android.util.DisplayMetrics
 import android.util.Log
 import android.view.View
 import android.view.WindowManager
